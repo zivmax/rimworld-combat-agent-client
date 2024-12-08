131a833fc0b6f637830a13034f13183f8575ee89
zivmax <tanglzh2022@shanghaitech.edu.cn>
2024-12-08 23:47:55 +0800

    feat: implement NotifyComp to display success messages and log mod loading

--- Source/Main.cs
+++ Source/Main.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using Verse.Sound;
+using Verse.Noise;
+using Verse.Grammar;
+using RimWorld;
+using RimWorld.Planet;
+
+namespace CombatAgent
+{
+    [DefOf]
+    public class LetterDefinitions
+    {
+        public static LetterDef success_letter;
+    }
+
+    public class NotifyComp : MapComponent
+    {
+        public NotifyComp(Map map) : base(map){}
+        public override void FinalizeInit()
+        {
+            Messages.Message("Success", null, MessageTypeDefOf.PositiveEvent);
+            Find.LetterStack.ReceiveLetter(new TaggedString("Success"), new TaggedString("Success message"), LetterDefinitions.success_letter, "", 0);
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+        static Start()
+        {
+            Log.Message("Combat Agent mod loaded successfully!");
+        }
+    }
+}
