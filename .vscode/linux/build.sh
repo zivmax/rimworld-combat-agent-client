3f0eabd568387366b390d7f19742763997325f51
zivmax <tanglzh2022@shanghaitech.edu.cn>
2024-12-11 18:49:31 +0800

    build: enhance build scripts for assembly management on Linux and Windows

--- .vscode/linux/build.sh
+++ .vscode/linux/build.sh
@@ -5,3 +5,9 @@ rm -rf ./*/Assemblies/*.*
 
 # Build DLL
 dotnet build .vscode/linux
+
+mkdir -p ../../RimWorldLinux_Data/Managed/AddedHistory
+
+find ./*/Assemblies -type f ! -name 'Combat.Agent.dll' -exec cp {} ../../RimWorldLinux_Data/Managed/AddedHistory \;
+
+cp ../../RimWorldLinux_Data/Managed/AddedHistory/* ../../RimWorldLinux_Data/Managed/
--- .vscode/win/build.bat
+++ .vscode/win/build.bat
@@ -1,7 +1,24 @@
 echo off
 
-REM remove unnecessary assemblies
+:: Remove unnecessary assemblies
 DEL .\*\Assemblies\*.*
 
-REM build dll
+:: Build dll
 dotnet build .vscode\win
+
+
+@echo off
+:: Create directory for managed assemblies
+if not exist "..\..\RimWorldWin64_Data\Managed\AddedHistory" (
+    mkdir "..\..\RimWorldWin64_Data\Managed\AddedHistory"
+)
+
+:: Find and copy assemblies, excluding Combat.Agent.dll
+for /r %%i in (.\*\Assemblies\*.dll) do (
+    if not "%%~nxi"=="Combat.Agent.dll" (
+        copy "%%i" "..\..\RimWorldWin64_Data\Managed\AddedHistory\"
+    )
+)
+
+:: Copy all files from AddedHistory to Managed folder
+xcopy /y "..\..\RimWorldWin64_Data\Managed\AddedHistory\*" "..\..\RimWorldWin64_Data\Managed\"
