7b00c6e702684628170b1ed72409df9dd46c9353
zivmax <tanglzh2022@shanghaitech.edu.cn>
2024-12-12 23:06:04 +0800

    fix: update PawnAction label property and clean up GameAction class

--- Source/GameAction.cs
+++ Source/GameAction.cs
@@ -1,29 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-
-using UnityEngine;
-using Verse;
-using Verse.AI;
-using Verse.AI.Group;
-using Verse.Sound;
-using Verse.Noise;
-using Verse.Grammar;
-using RimWorld;
-using RimWorld.Planet;
-using RimWorld.SketchGen;
 
 namespace CombatAgent
 {
+    [Serializable]
     public class PawnAction
     {
-        public string Lable { get; set; }
+        public string Label { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
     }
 
+    [Serializable]
     public class GameAction
     {
         public Dictionary<string, PawnAction> PawnActions { get; set; }
