14629d1e65dbf23829c00b2ca720728e995d22c9
mike2367 <yuguo2022@shanghaitech.edu.cn>
2024-12-20 17:20:51 +0800

    this branch is for 3 allies

--- Source/Config.cs
+++ Source/Config.cs
@@ -3,7 +3,7 @@ namespace CombatAgent
     public static class Config
     {
         public static bool AgentControlEnabled { get; set; } = true;
-        public static int TeamSize { get; set; } = 1;
+        public static int TeamSize { get; set; } = 3;
         public static int MapSize { get; set; } = 20;
         public static bool MapGenTrees { get; set; } = false;
         public static bool MapGenRuin { get; set; } = true;
