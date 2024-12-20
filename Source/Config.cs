b17677e7a4875dfa6c41dcb53616410804531a3f
zivmax <tanglzh2022@shanghaitech.edu.cn>
2024-12-20 16:14:32 +0800

    feat: enable ruin generation in map settings

--- Source/Config.cs
+++ Source/Config.cs
@@ -6,7 +6,7 @@ namespace CombatAgent
         public static int TeamSize { get; set; } = 1;
         public static int MapSize { get; set; } = 20;
         public static bool MapGenTrees { get; set; } = false;
-        public static bool MapGenRuin { get; set; } = false;
+        public static bool MapGenRuin { get; set; } = true;
         public static float Interval { get; set; } = 1.0f;
         public static int Speed { get; set; } = 1;
     }
