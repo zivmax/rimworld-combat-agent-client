5d29e3a4215f2f209079ca2a32945927190403c5
mike2367 <yuguo2022@shanghaitech.edu.cn>
2024-12-27 11:31:42 +0800

    refactor: alter the born pos for both party yo find optimal point

--- Source/Config.cs
+++ Source/Config.cs
@@ -2,7 +2,7 @@ namespace CombatAgent
 {
     public static class Config
     {
-        public static bool AgentControlEnabled { get; set; } = true;
+        public static bool AgentControlEnabled { get; set; } = false;
         public static int TeamSize { get; set; } = 1;
         public static int MapSize { get; set; } = 15;
         public static int RestartInterval { get; set; } = 300;
@@ -10,7 +10,7 @@ namespace CombatAgent
         public static bool MapGenRuin { get; set; } = true;
         public static int? RandomSeed { get; set; } = 4048;
         public static bool CanFlee { get; set; } = false;
-        public static bool ActivelyAttack { get; set; } = false;
+        public static bool ActivelyAttack { get; set; } = true;
         public static float Interval { get; set; } = 1.0f;
         public static int Speed { get; set; } = 1;
     }
